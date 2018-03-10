d73992fbad59bdd7cdcbfdedda897212a105f6ed
Bobur2277@gmail.com <Bobur@DESKTOP-R6KNSMQ.home>
2018-03-10 13:31:13 -0500

    assalom aleykum statement

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -5,5 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Class!");
 		System.out.println("add a line");
+		
+		System.out.println("assalom aleykum oka, ishal zor!!!");
+		
 	}
 }
